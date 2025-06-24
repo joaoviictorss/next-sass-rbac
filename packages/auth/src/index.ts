8eae43cb1612465b5a96ae27f581ac3b1ecab479
João Victor dos Santos Silva <joao.viictorss31@gmail.com>
2025-06-24 00:14:36 -0300

    feat: add permissions package with casl

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,53 @@
+import {
+  createMongoAbility,
+  CreateAbility,
+  MongoAbility,
+  AbilityBuilder,
+} from "@casl/ability";
+
+import { User } from "./models/user";
+
+import { permissions } from "./permissions";
+import { userSubject } from "./subjects/user";
+import { projectSubject } from "./subjects/project";
+import { organizationSubject } from "./subjects/organization";
+import { inviteSubject } from "./subjects/invite";
+import { billingSubject } from "./subjects/billing";
+import { z } from "zod";
+
+export * from "./models/user";
+export * from "./models/project";
+export * from "./models/organization";
+
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([z.literal("manage"), z.literal("all")]),
+]);
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>;
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+export const defineAbilityFor = (user: User) => {
+  const builder = new AbilityBuilder(createAppAbility);
+
+  if (typeof permissions[user.role] !== "function") {
+    throw new Error(`Permissions for role "${user.role}" not found`);
+  }
+
+  permissions[user.role](user, builder);
+
+  const ability = builder.build({
+    detectSubjectType(subject) {
+      return subject.__typename;
+    },
+  });
+
+  return ability;
+};
